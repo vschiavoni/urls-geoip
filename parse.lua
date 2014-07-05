899cf9437ff327b1084006f56406759481e76bc1
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-07-05 17:45:43 +0200

    remove useless code

--- parse.lua
+++ parse.lua
@@ -5,25 +5,13 @@ for line in io.lines() do
 	--print(line)
 	l_c=l_c+1	
 	local tokens=misc.split(line)
-	if tokens[1]=="#<struct" then
-		
+	if tokens[1]=="#<struct" then		
 		local idx_lat_start,idx_lat_stop=line:find('latitude=',1)
 		local idx_lat_end=line:find(",",idx_lat_stop)
-		local lat=line:sub(idx_lat_stop+1,idx_lat_end-1)
-		
+		local lat=line:sub(idx_lat_stop+1,idx_lat_end-1)		
 		local idx_long_start,idx_long_stop=line:find('longitude=',1) --from the beginning
 		local idx_long_end=line:find(",",idx_long_stop)
-		local long=line:sub(idx_long_stop+1,idx_long_end-1)		
-		
-		print(long,lat)
-		
-		----print(tokens[12],tokens[13])
-		--local lat=tokens[12]:match("%p(%p*%d+%p%d+)%p")
-		--local lon=tokens[13]:match("%p(%p*%d+%p%d+)%p")		
-		--if lat ==nil or  lon==nil then
-		--	print("Some error occurred while parsing line:"..line)
-		--else
-		--	print(lon,lat)		
-		--end		
+		local long=line:sub(idx_long_stop+1,idx_long_end-1)					
+		print(long,lat)	
 	end
 end
