8d8f61e67b76fcfb79afebf8bfd070acd823ef93
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-07-21 15:27:00 +0200

    add values for 256

--- parse_kmeans_clusters.lua
+++ parse_kmeans_clusters.lua
@@ -2,7 +2,7 @@ dofile("it-2004.sites.gpscoords.lua") --precomputed by: lua parse_latlong.lua
 assert(sites)
 dofile("analyze_clusters.lua")
 
-parts={16,32,64,128}
+parts={16,32,64,128,256}
 
 for _,p in pairs(parts) do
 	dofile("../k-means-geographic-clustering/k-means-result_k_"..p..".lua")
