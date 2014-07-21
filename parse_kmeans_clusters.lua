b7cb71c0803f14144758d9cb939cf6118a1428b4
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-07-21 15:19:46 +0200

    launch analyze_clusters on all kmeans clusters

---
+++ parse_kmeans_clusters.lua
@@ -0,0 +1,13 @@
+dofile("it-2004.sites.gpscoords.lua") --precomputed by: lua parse_latlong.lua
+assert(sites)
+dofile("analyze_clusters.lua")
+
+parts={16,32,64,128}
+
+for _,p in pairs(parts) do
+	dofile("../k-means-geographic-clustering/k-means-result_k_"..p..".lua")
+	assert(clusters)
+	analyze(clusters)
+	
+	clusters=nil 
+end
