0006a4c1978325ebccdb33f51d69da2bff459bb3
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-07-21 14:21:40 +0200

    compute all distances beteween coordinates, in case needed

---
+++ alldistances.lua
@@ -0,0 +1,30 @@
+--[[
+Compute the all-pairs distance between the coordiantes.
+This code is serial, and it takes approx 3h30min to complete on the input coordantes.
+Once parallelized (using LuaLanes or others), we should observe massive speedup on multi-core machines.
+
+--]]
+require"os"
+local sin, asin, cos, sqrt, rad = math.sin, math.asin, math.cos, math.sqrt, math.rad
+function haversine_distance(a_lon, a_lat, b_lon, b_lat)
+	local radius = 6371.0
+	local lon_d = rad((b_lon - a_lon))
+	local lat_d = rad((a_lat - a_lat))    
+	local t1 = sin(lat_d / 2)^2 + cos(rad(a_lat)) * cos(rad(b_lat)) * sin(lon_d/2)^2
+	local t2 = 2 * asin(sqrt(t1))
+	return radius * t2
+end
+dofile("it-2004.sites.gpscoords.lua")
+x = os.clock()
+for i=1,#sites do
+	if sites[i]~=nil then
+		for j=2,#sites do
+			if sites[j]~=nil then
+				haversine_distance(sites[i][1], sites[i][2], sites[j][1], sites[j][2])
+			end
+		end
+	end
+	print(string.format("elapsed time: %.2f", os.clock() - x),i.."/"..#sites)
+	
+	--print(string.format("elapsed time: %.2f\n", (os.clock() - x), "Done: ",i.."/"..#sites)
+end
