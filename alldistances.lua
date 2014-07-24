beac1bc8171da21de327e1657a9338a9d42fd354
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-07-24 18:10:08 +0200

    use dr2 constant instead of math.rad

--- alldistances.lua
+++ alldistances.lua
@@ -3,20 +3,22 @@ Compute the all-pairs distance between the coordiantes.
 --]]
 require"os"
 local sin, asin, cos, sqrt, rad = math.sin, math.asin, math.cos, math.sqrt, math.rad
+local dr2 = 0.0174532925199433 --replace calls to math.rad(..)
 function haversine_distance(a_lon, a_lat, b_lon, b_lat)
 	local radius = 6371.0
-	local lon_d = rad((b_lon - a_lon))
-	local lat_d = rad((a_lat - a_lat))    
-	local t1 = sin(lat_d / 2)^2 + cos(rad(a_lat)) * cos(rad(b_lat)) * sin(lon_d/2)^2
+	local lon_d = dr2*(b_lon - a_lon)
+	local lat_d = dr2*(a_lat - a_lat)    
+	local t1 = sin(lat_d / 2)^2 + cos(dr2*(a_lat)) * cos(dr2*(b_lat)) * sin(lon_d/2)^2
 	local t2 = 2 * asin(sqrt(t1))
 	return radius * t2
 end
 --the dofile imports the sites table in the global namespace
-os.setlocale("C")
 dofile("it-2004.sites.gpscoords-0_65039.lua")
+io.stderr:write(#sites,"\n")
 dofile("it-2004.sites.gpscoords-65040_132020.lua")
+io.stderr:write(#sites,"\n")
 dofile("it-2004.sites.gpscoords-132021_141252.lua")
-io.stdout:write(#sites,"\n")
+io.stderr:write(#sites,"\n")
 x = os.clock()
 for i=1,#sites do
 	local i_dist={}
